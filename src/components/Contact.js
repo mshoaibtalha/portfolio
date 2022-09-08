f9e8b7cc6876324a8085e00858209457f963bb7a
mshoaibtalha <mshoaibtalha@gmail.com>
2022-09-08 13:35:17 +0200

    made form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,7 +16,7 @@ const Contact = () => {
 
         <div className=' flex justify-center items-center'>
           <form
-            action='https://getform.io/f/61c99527-2b15-42cf-9b55-ad37d2f7daa6'
+            action='https://getform.io/f/e893c144-6e66-4301-880d-796f1fd0668b'
             method='POST'
             className=' flex flex-col w-full md:w-1/2'
           >
@@ -24,19 +24,19 @@ const Contact = () => {
               type='text'
               name='name'
               placeholder='Enter your name'
-              className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'
+              className='p-2 bg-transparent border-2 rounded-md text-black focus:outline-none'
             />
             <input
               type='text'
               name='email'
               placeholder='Enter your email'
-              className='my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'
+              className='my-4 p-2 bg-transparent border-2 rounded-md text-black focus:outline-none'
             />
             <textarea
               name='message'
               placeholder='Enter your message'
               rows='10'
-              className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'
+              className='p-2 bg-transparent border-2 rounded-md text-black focus:outline-none'
             ></textarea>
 
             <button className='text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300'>
